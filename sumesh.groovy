ac111c282c58db5ee188d7ac8e167fbaefe80de2
sumeshkanayi <sumeshkanayi@gmail.com>
2017-10-22 11:30:15 +0530

    Create sumesh.groovy

---
+++ sumesh.groovy
@@ -0,0 +1,15 @@
+freeStyleJob('myFree') {
+  description("myjob")
+  
+  
+}
+freeStyleJob('myFree2') {
+  description("myjob2")
+  
+  
+}
+freeStyleJob('myFree3') {
+  description("myjob3")
+  
+  
+}
